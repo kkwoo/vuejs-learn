3643951aed9aec6c9aab5fc2e250a59d5ef8d25b
kkwoo <kevin.k.woo@gmail.com>
2020-12-19 11:27:57 +0000

    Button now toggles timer

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ const CounterApp = {
   data() {
     return {
       counter: 0,
-      timer: ""
+      timer: "",
+      timerStatusToggle: "Start"
     }
   }, 
+  methods: {
+    stopTimer() {
+      clearInterval(this.timer);
+      this.timer = "";
+      this.timerStatusToggle = "Start";
+    },
+    startTimer() {
+      this.counter = 0;
+      this.timer = setInterval(() => {
+        this.counter++
+      }, 1000);
+      this.timerStatusToggle = "Stop";
+    },
+    toggleTimer() {
+      (this.timer === "") ? this.startTimer() : this.stopTimer();
+    }
+  },
   mounted() {
-    timer = setInterval(() => {
-      this.counter++
-    }, 1000)
+    this.startTimer();
   }
 }
 
