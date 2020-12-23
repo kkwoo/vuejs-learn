cb05e64dbe0742f92b988b136949d81f6aaa6c77
kkwoo <kevin.k.woo@gmail.com>
2020-12-23 05:41:02 +0000

    increase gapi scope to r/w calendar access

--- gapiHelpers.js
+++ gapiHelpers.js
@@ -11,7 +11,7 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+var SCOPES = "https://www.googleapis.com/auth/calendar";
 
 var authorizeButton = document.getElementById('authorize_button');
 var signoutButton = document.getElementById('signout_button');
